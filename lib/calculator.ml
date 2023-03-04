c1d1c21d52b3783db18dd89ab0fe006f3cc7cac1
Paul Chobert <paul@chobert.fr>
2023-03-04 21:28:25 +0100

    Handle multiple delimiters

--- lib/calculator.ml
+++ lib/calculator.ml
@@ -3,20 +3,28 @@ open Core
 exception NoNegative of int list
 
 module Utils = struct
-  let re =
+  let expr_re =
     Re2.create_exn
       ~options:{ Re2.Options.default with dot_nl = true }
-      {|(//\[([^]]+)\]\n)?(.*)$|}
+      {|((?P<delimiters>//(\[(.*)\]+))\n)?(?P<numbers>.*)$|}
 
+  let delimiter_re = Re2.create_exn {|\[([^]]+)\]|}
   let int_opt_of_string str = try Some (Int.of_string str) with _ -> None
 
   let sum str =
-    let m = Re2.first_match_exn re str in
-    let delim = Re2.Match.get ~sub:(`Index 2) m |> Option.value ~default:"," in
-    let numbers = Re2.Match.get_exn ~sub:(`Index 3) m in
+    let m = Re2.first_match_exn expr_re str in
+    let delimiters =
+      Re2.Match.get ~sub:(`Name "delimiters") m
+      |> Option.value ~default:"[,]"
+      |> Re2.get_matches_exn delimiter_re
+      |> List.map ~f:(fun m -> Re2.Match.get_exn m ~sub:(`Index 1))
+    in
+    let numbers = Re2.Match.get_exn ~sub:(`Name "numbers") m in
     let positive, negative =
-      numbers |> String.split ~on:'\n'
-      |> List.concat_map ~f:(String.split_on_chars ~on:(String.to_list delim))
+      List.fold ("\n" :: delimiters) ~init:[ numbers ]
+        ~f:(fun numbs delimiter ->
+          List.concat_map numbs
+            ~f:(String.split_on_chars ~on:(String.to_list delimiter)))
       |> List.filter_map ~f:int_opt_of_string
       |> List.filter ~f:(( >= ) 1000)
       |> List.partition_tf ~f:(fun n -> n > 0)
@@ -69,6 +77,14 @@ module Utils = struct
 
   let%expect_test "//[***]\n1***2***3" =
     printf "%d" (sum "//[***]\n1***2***3");
+    [%expect {| 6 |}]
+
+  let%expect_test "//[*][%]\n1*2%3" =
+    printf "%d" (sum "//[*][%]\n1*2%3");
+    [%expect {| 6 |}]
+
+  let%expect_test "//[**][%%]\n1**2%%3" =
+    printf "%d" (sum "//[**][%%]\n1**2%%3");
     [%expect {| 6 |}]
 end
 
